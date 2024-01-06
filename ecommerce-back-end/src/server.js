7d58c2505a9c39d735dd01f4440fccba0e67713a
drakenevadie19 <drakennievadje19@gmail.com>
2024-01-06 07:23:27 -0600

    New Change: Optimize shopping cart array by only storing ids, and adapt GET, POST, DELETE endpoints corresponding to cartItem

--- ecommerce-back-end/src/server.js
+++ ecommerce-back-end/src/server.js
@@ -2,27 +2,34 @@ import express from 'express';
 
 import { cartItems as cartItemsRaw, products as productsRaw, } from './temp-data';
 
+let cartItem = cartItemsRaw;
+let productItem = productsRaw;
+
+
 const app = express();
 
-// GET endpoint sample
-app.get('/hello', (req, res) => {
-    res.send('This is GET request!');
-})
+// Function to convert all ids of items to products 
+//  => if we assign this function to a variable, we can get an array/list of products
+function populatedCartIds(ids) {
+    return ids.map(id => productItem.find(product => product.id === id));
+}
 
 // Loading list of products
 app.get('/products', (req, res) => {
-    res.json(products);
+    res.json(productItem);
 })
 
 // Loading user's current shopping cart
 app.get('/cart', (req, res) => {
-    res.json(cartItems);
+    // Use map function to map id to corresponding product
+    const populatedCart = populatedCartIds(cartItem);
+    res.json(populatedCart);
 })
 
 // Loading list of products
 app.get('/products/:productId', (req, res) => {
     const productId = req.params.productId;
-    const product = products.find(product => product.id === productId);
+    const product = productItem.find(product => product.id === productId);
 
     if (product) {
         res.json(product)
@@ -32,10 +39,6 @@ app.get('/products/:productId', (req, res) => {
     }
 })
 
-
-let cartItem = cartItemsRaw;
-let productItem = productsRaw;
-
 app.use(express.json());
 
 // Add an item to shopping cart
@@ -43,10 +46,23 @@ app.post('/cart', (req, res) => {
     // In this POST request, we have add item's id to request body => We can get that id 
     const productId = req.body.id;
     // Find that product
-    const product = productItem.find(product => product.id === productId);
+    //  const product = productItem.find(product => product.id === productId);
     // Once we found that product, insert it to cart
-    cartItem.push(product);
-    res.json(cartItem);
+    cartItem.push(productId);
+    
+    // An array of products corresponding to current shopping cart 
+    const populatedCart = populatedCartIds(cartItem);
+    res.json(populatedCart);
+})
+
+// Remove item from cart 
+app.delete('/cart/:productId', (req, res) => {
+    const productId = req.params.productId;
+    // Since we have id of item to remove, we will filter out all ids in cartItem match productId
+    //  => cartItem array only contain ids that does not match productId
+    cartItem = cartItem.filter(cartitemId => cartitemId !== productId);
+    const populatedCart = populatedCartIds(cartItem);
+    res.json(populatedCart);
 })
 
 // Server listen to request
--- ecommerce-back-end/src/temp-data.js
+++ ecommerce-back-end/src/temp-data.js
@@ -7,26 +7,7 @@
 // import mediumBottle from './assets/products/medium-bottle.webp';
 // import boldBottle from './assets/products/bold-bottle.webp';
 
-export const cartItems = [
-    {
-        id: '123',
-        name: 'Basil',
-        price: '$26.00',
-        // imageName: basilBottle,
-    }, 
-    {
-        id: '234',
-        name: 'Lemon',
-        price: '$26.00',
-        // imageName: lemonBottle,
-    }, 
-    {
-        id: '345',
-        name: 'Garlic',
-        price: '$26.00',
-        // imageName: garlicBottle,
-    }
-];
+export const cartItems = ['123','678','345'];
 
 export const products = [{
     id: '123',
