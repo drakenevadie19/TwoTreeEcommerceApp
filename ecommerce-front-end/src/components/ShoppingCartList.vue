b7a22f84f78c575181deeeb6da9a04ae09b9758d
drakenevadie19 <drakennievadje19@gmail.com>
2024-01-07 10:59:17 -0600

    New change
    : use Hashmap to map the same item in shopping cart, but still have problem to display them

--- ecommerce-front-end/src/components/ShoppingCartList.vue
+++ ecommerce-front-end/src/components/ShoppingCartList.vue
@@ -7,6 +7,8 @@
                 <p>{{ product.price }}</p>
             </div>
 
+            <div class="product-amount">{{ this.map.get(product.id) }}</div>
+
             <!-- Button for removing item from cart  -->
             <!-- Emit an event for parent component (ShoppingCartPage) to listen when remove item from cart  -->
             <button @click="$emit('remove-from-cart', product.id)" class="remove-button">Remove item from cart</button>
@@ -19,7 +21,42 @@
 
 <script>
     export default {
-        name: "ShoppingCartList", 
-        props: ['cartList'],
-    }
+    name: "ShoppingCartList",
+    props: ['cartList'],
+    data() {
+        return {
+            map: new Map(), // Initialize a new Map
+        };
+    },
+    watch: {
+        // Watch for changes in the cartList prop
+        cartList: {
+        handler: function (newCartList) {
+            // Update the hash map when the cartList prop changes
+            this.mapItem(newCartList);
+        },
+        immediate: true, // Trigger the handler immediately on component creation
+        },
+    },
+    methods: {
+        mapItem(cartList) {
+        // Reset the map before updating to avoid accumulation of counts
+        this.map.clear();
+
+        for (let item of cartList) {
+            if (this.map.has(item.id)) {
+            let temp = this.map.get(item.id);
+            temp++;
+            this.map.set(item.id, temp);
+            } else {
+            this.map.set(item.id, 1);
+            }
+        }
+        },
+    },
+    created() {
+        // Initialize the hash map when the component is created
+        this.mapItem(this.cartList);
+    },
+    };
 </script>
