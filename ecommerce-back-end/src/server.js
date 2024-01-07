bcf3e2c84e3af4307e32d18c1d515b41a3de507e
drakenevadie19 <drakennievadje19@gmail.com>
2024-01-07 02:22:12 -0600

    New Change: Fix the GET endpoint for individual product

--- ecommerce-back-end/src/server.js
+++ ecommerce-back-end/src/server.js
@@ -7,79 +7,78 @@ import { MongoClient } from 'mongodb';
 let cartItem = cartItemsRaw;
 let productItem = productsRaw;
 
-const url = `mongodb+srv://thanhnguyen14gers:Ec9gdaFqjGaZWcvF@cluster0.nve8zrs.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(url);
+async function startTheServer() {
+    const client = new MongoClient(url);
 
-const app = express();
-
-app.use(express.json());
-
-// Function to convert all ids of items to products 
-//  => if we assign this function to a variable, we can get an array/list of products
-async function populatedCartIds(ids) {
+    // Connect to the db and select the db 
     await client.connect();
     const db = client.db('ecomnerce-app');
+    
+    // FROM NOW ON, WE CAN USE db variable FOR database querries
 
-    return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
+    const app = express();
+    app.use(express.json());
+  
+
+    // Function to convert all ids of items to products 
+    //  => if we assign this function to a variable, we can get an array/list of products
+    async function populatedCartIds(ids) {
+        return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
+    }
+
+    // Loading list of products
+    app.get('/products', async (req, res) => {
+        // Connect to the client 
+
+        const products = await db.collection('products').find({}).toArray();
+        res.send(products);
+    })
+
+    // Loading user's current shopping cart
+    app.get('/users/:userId/cart', async (req, res) => {
+        // Get the users from db
+        const user = await db.collection('users').findOne({ id: req.params.userId });
+        const populatedCart = await populatedCartIds(user.cartItems);
+        res.json(populatedCart);
+    })
+
+    // Loading list of products
+    app.get('/products/:productId', async (req, res) => {
+        // Connecting the db and select the db
+        const productId = req.params.productId;
+        const product = await db.collection('products').findOne({ id: productId });
+
+        res.json(product)
+    })
+
+    // Add an item to shopping cart
+    app.post('/cart', (req, res) => {
+        // In this POST request, we have add item's id to request body => We can get that id 
+        const productId = req.body.id;
+        // Find that product
+        //  const product = productItem.find(product => product.id === productId);
+        // Once we found that product, insert it to cart
+        cartItem.push(productId);
+        
+        // An array of products corresponding to current shopping cart 
+        const populatedCart = populatedCartIds(cartItem);
+        res.json(populatedCart);
+    })
+
+    // Remove item from cart 
+    app.delete('/cart/:productId', (req, res) => {
+        const productId = req.params.productId;
+        // Since we have id of item to remove, we will filter out all ids in cartItem match productId
+        //  => cartItem array only contain ids that does not match productId
+        cartItem = cartItem.filter(cartitemId => cartitemId !== productId);
+        const populatedCart = populatedCartIds(cartItem);
+        res.json(populatedCart);
+    })
+
+    // Server listen to request
+    app.listen(8000, () => {
+        console.log('Server is listening on port 8000');
+    })
 }
 
-// Loading list of products
-app.get('/products', async (req, res) => {
-    // Connect to the client 
-    await client.connect();
-    const db = client.db('ecomnerce-app');
-    const products = await db.collection('products').find({}).toArray();
-    res.send(products);
-})
-
-// Loading user's current shopping cart
-app.get('/users/:userId/cart', async (req, res) => {
-    await client.connect();
-    const db = client.db('ecomnerce-app');
-
-    // Get the users from db
-    const user = await db.collection('users').findOne({ id: req.params.userId });
-    const populatedCart = await populatedCartIds(user.cartItems);
-    res.json(populatedCart);
-})
-
-// Loading list of products
-app.get('/products/:productId', (req, res) => {
-    const productId = req.params.productId;
-    const product = productItem.find(product => product.id === productId);
-
-    if (product) {
-        res.json(product)
-    } else {
-        res.sendStatus(404);
-        console.log('product with id=' + productId + ' not found');
-    }
-})
-// Add an item to shopping cart
-app.post('/cart', (req, res) => {
-    // In this POST request, we have add item's id to request body => We can get that id 
-    const productId = req.body.id;
-    // Find that product
-    //  const product = productItem.find(product => product.id === productId);
-    // Once we found that product, insert it to cart
-    cartItem.push(productId);
-    
-    // An array of products corresponding to current shopping cart 
-    const populatedCart = populatedCartIds(cartItem);
-    res.json(populatedCart);
-})
-
-// Remove item from cart 
-app.delete('/cart/:productId', (req, res) => {
-    const productId = req.params.productId;
-    // Since we have id of item to remove, we will filter out all ids in cartItem match productId
-    //  => cartItem array only contain ids that does not match productId
-    cartItem = cartItem.filter(cartitemId => cartitemId !== productId);
-    const populatedCart = populatedCartIds(cartItem);
-    res.json(populatedCart);
-})
-
-// Server listen to request
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
-})
+startTheServer();
